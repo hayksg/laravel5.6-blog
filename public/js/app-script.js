b411beae82e36b74547be16a997a5b9964990493
vagrant <vagrant@laravel>
2018-04-03 13:35:33 +0000

    Last commit

--- public/js/app-script.js
+++ public/js/app-script.js
@@ -118,8 +118,24 @@ $(function(){
         }
     });
     
+    // Back to top
+    
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+	
+    // scroll body to 0px on click
+    $('#back-to-top').click(function () {			
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
+    
     //
-    
-    
    
 });
--- resources/views/layouts-site/footer.blade.php
+++ resources/views/layouts-site/footer.blade.php
@@ -1,8 +1,6 @@
    <footer class="blog-footer footer">
       <p>© 2017 - 2018 by PHP-User. All rights reserved.</p>
-      <p>
-        <a href="#">Back to top</a>
-      </p>
+      <p><a href="#" id="back-to-top">Back to top</a></p>
     </footer>
 
     <script src="/js/jquery.js" type="text/javascript"></script>
